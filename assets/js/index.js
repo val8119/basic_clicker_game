b349fdd8fd7244e082950f0f45a1f69dbe1c7882
val8119 <mikk.vahaste@gmail.com>
2020-07-05 02:23:10 +0300

    Added reset button

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,24 +1,32 @@
 // selectors
 const circle = document.querySelector(".container");
 const scoreText = document.querySelector(".score");
+const resetBtn = document.querySelector(".reset-btn");
 var score;
 
 // listeners
-document.addEventListener("DOMContentLoaded", getScore);
+document.addEventListener("DOMContentLoaded", scoreGet);
 circle.addEventListener("click", scoreAdd);
+resetBtn.addEventListener("click", scoreReset);
 
 // functions
+function scoreReset() {
+    score = 0;
+    localStorage.setItem("score", score);
+    scoreText.innerText = `points: ${score}`;
+}
+
 function scoreAdd() {
     score++;
     localStorage.setItem("score", score);
-    scoreText.innerText = `points: ${score}`
+    scoreText.innerText = `points: ${score}`;
 }
 
-function getScore() {
+function scoreGet() {
     if (localStorage.getItem("score") === null) {
         score = 0
     } else {
         score = localStorage.getItem("score");
     }
-    scoreText.innerText = `points: ${score}`
+    scoreText.innerText = `points: ${score}`;
 }
